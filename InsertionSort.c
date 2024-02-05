fa51c4a2d514da759dce0348498ae6a1d9eaf0ef
Azzathoth1126 <alexis.mta2020@gmail.com>
2024-02-05 02:04:54 +0000

    Codigo

---
+++ InsertionSort.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    double list[] = {2, 3, 6, 1, 4, 8, 5, 9, 7, 10};
+    int n = 10;
+    printf("-----Código de ordenamiento-----\n");
+    printf("---------Insertion Sort---------\n");
+    
+    for(int i = 1; i < n; i++) {
+        double index = list[i];
+        int j = i - 1;
+        
+        while(j >= 0 && list[j] > index) {
+            list[j + 1] = list[j];
+            j = j - 1;
+        }
+        list[j + 1] = index;
+    }
+    
+    printf("{");
+    for(int a = 0; a < 10; a++) {
+        printf("%.0f", list[a]);
+        if (a < 9) {
+            printf(", ");
+        }
+    }
+    printf("}\n");
+    
+    return 0;
+}
