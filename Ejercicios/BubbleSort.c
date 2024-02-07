4566458375405348a34ae0ab95cf2e0a0ca3cc04
Azzathoth1126 <alexis.mta2020@gmail.com>
2024-02-07 19:49:21 +0000

    listo

---
+++ Ejercicios/BubbleSort.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+    
+    int list[] = {2, 3, 6, 1, 4, 8, 5, 9, 7, 10};
+    int n = 10;
+    int i = n;
+    bool ver = true;
+
+    printf("'\t-----Código de ordenamiento-----\n");
+    printf("\t-----------Bubble Sort-----------\n");
+    
+    while(i>=1 && ver == true) {
+        ver = false;
+        for(int j=0; j<n; j++) {
+            if(list[j]>list[j+1]) {
+                
+                int a = list[j];
+                list[j] = list[j+1];
+                list[j+1] = a;
+                ver = true;
+            }
+        }
+        i = i-1;
+    }
+    
+    // for(int x=0; x<n; x++) {
+    //         printf("%d ", list[x]);
+    //     }
+
+    printf("\t\n\n{");
+    for(int a = 0; a < 10; a++) {
+        printf("%.d", list[a]);
+        if (a < 9) {
+            printf(", ");
+        }
+    }
+    printf("}\n");
+    
+    return 0;
+}
