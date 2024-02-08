949c2c46a7c532740ffafcae0f72453482ca4634
Azzathoth1126 <alexis.mta2020@gmail.com>
2024-02-08 23:57:25 +0000

    listo

--- Ejercicio_01/pruebas.c
+++ Ejercicio_01/pruebas.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
-
+//Primera prueba lista - lista
 void selectionSort(int arreglo[], int n);
 void printArray(int arr[],int size);
+void swap(int* a, int* b);
+//Segunda prueba - lista
+void insertionSort(int a[], int n);
+//Tercera prueba 
+void bubbleSort(int a[], int n);
 
 int main(){
 
-    int arrelgo[] = {3,21,7,2,95,23,1,0};
-    int n = sizeof(arreglo)/sizeof(list[0]);
+    int arreglo[] = {3,21,7,2,95,23,1,0};
+    int n = sizeof(arreglo)/sizeof(arreglo[0]);
 
-    selectionSort(arreglo, tam);
-
+    //selectionSort(arreglo, n);
+    //insertionSort(arreglo, n);
+    
     return 0;
 }
 
@@ -30,9 +36,33 @@ void selectionSort(int arreglo[], int n){
 	}
 }
 
+void insertionSort(int a[], int n){
+	int i,j,k;
+	int aux;
+
+	for(i=1; i<n; i++){
+		j=i;
+		aux=a[i];
+		while (j>0 && aux < a[j-1]){
+			a[j] = a[j-1];
+			j--;
+		}
+		a[j]=aux;
+
+		printf("\nIteracion numero %d \n",i);
+		printArray(a,n);
+	}
+}
+
 void printArray(int arr[],int size){
 	int i;
   	for (i=0; i < size; i++)
        	printf("%d ", arr[i]);
     	printf("\n");
+}
+
+void swap(int* a, int* b){
+    int t = *a;
+    *a = *b;
+    *b = t;
 }
