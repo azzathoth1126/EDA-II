9b5e4a7fe44e59693f3bfa5b42baa550c42f85ce
Azzathoth1126 <alexis.mta2020@gmail.com>
2024-02-19 19:45:26 +0000

    Revisando y arreglando codigo

--- Practica02/Ejercicio_01/ejercicio2.c
+++ Practica02/Ejercicio_01/ejercicio2.c
@@ -154,13 +154,12 @@ void bubbleSort(int a[], int n) {
 
 //Agragado en el laboratorio
 void quickSort(int arr[], int low, int high) {
-    if (low < high)
-    {
+    if (low < high) {
         int pi = partition(arr, low, high);
         printSubArray(arr,low,pi-1);
-	  quickSort(arr, low, pi - 1);
+		quickSort(arr, low, pi - 1);
         printSubArray(arr,pi+1,high);
-	  quickSort(arr, pi + 1, high);
+	  	quickSort(arr, pi + 1, high);
     }
 }
 
@@ -169,11 +168,10 @@ int partition (int arr[], int low, int high) {
    	int pivot = arr[high];    
 	printf("Pivote: %d   \n ",pivot);
 	int j,i = (low - 1);  
-   	for (j = low; j <= high- 1; j++){
-       	if (arr[j] <= pivot){
+   	for (j = low; j <= high- 1; j++) {
+       	if (arr[j] <= pivot) {
             		i++;    
             		swap(&arr[i], &arr[j]);
-					
         	}
     	}
     	swap(&arr[i + 1], &arr[high]);
