01f2b5248488c083a9bf92b8efb0e1c5c11be850
Azzathoth1126 <alexis.mta2020@gmail.com>
2024-02-22 23:58:49 +0000

    Comentando el codigo

--- Practica03/Ejercicio01/Principal.java
+++ Practica03/Ejercicio01/Principal.java
@@ -1,23 +1,25 @@
-import java.util.Scanner;
+import java.util.Scanner; //Librerias para ingresar datos desde teclado
 
 public class Principal{
 
     public static void main(String args[]){
 
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in); //Crea un objeto para ingresar datos desde el teclado
 
-        char[] letras = new char[20];
-        int tam = letras.length; 
+        char[] letras = new char[20]; //Crea un arreglo de 20 espacios
+        int tam = letras.length; //Tamaño del arreglo
 
+        //Llena el arreglo
         System.out.println("Dame las letras del arreglo");
         for (int i =0; i<tam; i++){
             System.out.println("Elemento: " + (i + 1) + ": ");
             letras [i] = scanner.nextLine().charAt(0);
         }
 
+        //Imprime el arreglo
         System.out.println("\nEl arreglo es: ");
         for(char letra:letras){
-            System.out.println(letra + " \t");
+            System.out.println(letra + " ");
         }
 
 
