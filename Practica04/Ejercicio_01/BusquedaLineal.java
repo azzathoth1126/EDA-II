9151c70b6660ede9c4cd80955bdced70e69997c4
Azzathoth1126 <alexis.mta2020@gmail.com>
2024-03-01 00:36:03 +0000

    mejorando el codigo

--- Practica04/Ejercicio_01/BusquedaLineal.java
+++ Practica04/Ejercicio_01/BusquedaLineal.java
@@ -6,21 +6,16 @@ public class BusquedaLineal {
 
     public static void VerFal(List<Integer> lista, int elemSearch) {
         int com = 0;
-        
-        for (int elemento: lista) {
-            if (elemento == elemSearch) com++;
-        }
 
-        if (com == 1){
-            System.out.println("True");
-        }
-        else{
-            System.out.println("False");
-        }
+        for (int elemento: lista) if (elemento == elemSearch) com++;
+        
+        if (com == 1) System.out.println("True");    
+        else System.out.println("False");
     }
     
     public static void Indices (List<Integer> lista, int elemSearch) {
-
+        
+        //for()
 
     }
 
