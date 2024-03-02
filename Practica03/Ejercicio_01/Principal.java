affddb71a0400165c572fb26cd10e9533fd1af6d
Azzathoth1126 <alexis.mta2020@gmail.com>
2024-03-02 22:43:54 +0000

    Funcion de Llenado de arreglo listo

--- Practica03/Ejercicio_01/Principal.java
+++ Practica03/Ejercicio_01/Principal.java
@@ -5,18 +5,16 @@ public class Principal{
     public static void main(String args[]){
 
         Scanner scanner = new Scanner(System.in); //Crea un objeto para ingresar datos desde el teclado
-
+    
         char[] letras = new char[20]; //Crea un arreglo de 20 espacios
         int tam = letras.length; //Tamaño del arreglo
 
         //Llena el arreglo
-        System.out.println("Dame las letras del arreglo");
-        for (int i = 0; i < tam; i++){
-            System.out.println("Elemento: " + (i + 1) + ": ");
-            letras [i] = scanner.nextLine().charAt(0);
-        }
+        Utilerias.LLenadoArray(letras);
+        
 
-        CountingSort.CountingSort(letras, tam);
+        //CountingSort.CountingSort(letras, tam);
+
 
         //Imprime el arreglo
         Utilerias.PrintArray(letras, tam);
--- Practica03/Ejercicio_01/Utilerias.java
+++ Practica03/Ejercicio_01/Utilerias.java
@@ -3,6 +3,7 @@ import java.util.Scanner; //Solo llama a scane
 public class Utilerias {
 
 
+    //Imprime el Arreglo
     public static void PrintArray (char letras[], int tam) {
         
         System.out.println("\n\nArreglo: ");
@@ -13,7 +14,15 @@ public class Utilerias {
 
     }
 
-
-    //public static void FillArray (char letras[], int tam[]) {}
+    //LLena el arreglo de forma manual por el usuario
+    public static void LLenadoArray (char letras[]) {
+        Scanner scanner = new Scanner(System.in);//Para llamar al metodo nextLine de scanner
+    
+        System.out.println("Dame las letras del arreglo");
+        for (int i = 0; i < 20; i++){
+            System.out.println("Letra # " + (i + 1) + ": ");
+            letras [i] = scanner.nextLine().charAt(0);
+        }
+    }
 
 }
