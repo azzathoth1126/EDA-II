b4d3ed2c56e52e12759f4d0a4512bd6e290493ab
Azzathoth1126 <alexis.mta2020@gmail.com>
2024-03-06 00:00:04 +0000

    Reparando Radix

--- Practica03/Ejercicio_01/RadixSort.java
+++ Practica03/Ejercicio_01/RadixSort.java
@@ -5,36 +5,35 @@ import java.util.Random;
 public class RadixSort {
 
     public static void radixSort(int numeros[], int tam) {
-        Stack<Integer>[] digitStacks = new Stack[10];
-        for (int i = 0; i < 10; i++) {
-            if (i >= 3 && i <= 6) {
-                digitStacks[i] = new Stack<>();
-            }
+        Stack<Integer>[] digitStacks = new Stack[15];
+        int exp = 1;
+
+        for (int i = 0; i < 4; i++) {
+            digitStacks[i] = new Stack<>();
+            System.out.println("Pila "+ i + " " + digitStacks[i] + "\n");
         }
 
-        for (int exp = 1; tam / exp > 0; exp *= 10) {
+        for (int j=0; j<4; j++) {
             // Coloca los elementos en las pilas según el dígito actual
             for (int num : numeros) {
                 int digit = (num / exp) % 10;
-                digitStacks[digit].push(num);
+                digitStacks[digit - 3].push(num);
             }
-            //Utilerias.PrintArrays(numeros, 20);
-        }
 
-        for (int i =0; i<4; i++) {
-            if (stack != null) { // Verifica si la pila está inicializada
-                    while (!stack.isEmpty()) {
-                        numeros[i++] = stack.pop();
-                    }
-            }
-        }
-        int index = 0;
+            for(int i=0; i<4; i++) System.out.println("Pila "+ i + " con elementos: " + digitStacks[i]);
+            
             for (Stack<Integer> stack : digitStacks) {
+                int index = 15;
                 if (stack != null) { // Verifica si la pila está inicializada
                     while (!stack.isEmpty()) {
-                        numeros[index++] = stack.pop();
+                        numeros[--index] = stack.pop();
                     }
                 }
             }
+
+            System.out.println("\nArreglo modificado: ");
+            Utilerias.PrintArrays(numeros, tam);
+            exp = exp*10;
+        }
     }
 }
