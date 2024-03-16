8b60082da8743516e40b8e0246f31a51a9d5d7b9
Azzathoth1126 <alexis.mta2020@gmail.com>
2024-03-16 18:49:15 +0000

    Modificando codigos para analizar su complejidad

--- Proyecto01/Insercion.java
+++ Proyecto01/Insercion.java
@@ -1,6 +1,6 @@
 public class Insercion{  
     
-    public static void insertionSort(int array[]) {  
+    /*public static void insertionSort(int array[]) {  
         int iteraciones = 0, comparaciones = 0, cambios = 0, inserciones = 0;
 
         int n = array.length;  
@@ -24,5 +24,31 @@ public class Insercion{
         System.out.println("Inserciones: " + inserciones);
 
     
-    } 
+    }*/
+
+    public static void insertionSort(int array[]) {  
+        int accesosArreglo = 0, comparaciones = 0, cambios = 0, inserciones = 0;
+
+        int n = array.length;  
+        for (int j = 1; j < n; j++) {  
+            int key = array[j];
+            accesosArreglo++;  // Incremento cada vez que se accede al arreglo
+            
+            int i = j - 1;  
+            while ((i > -1) && (array[i] > key)) {  
+                comparaciones++; // Incremento por cada comparación de elementos del arreglo
+                array[i + 1] = array[i];  
+                cambios++; // Incremento por cada cambio de elementos
+                inserciones++; // Incremento por cada cambio o inserción de un elemento del arreglo
+                i--;  
+            }  
+            array[i + 1] = key;
+            inserciones++; // Incremento por cada inserción realizada
+        }  
+
+        System.out.println("Accesos al arreglo: " + accesosArreglo);
+        System.out.println("Comparaciones: " + comparaciones);
+        System.out.println("Cambios: " + cambios);
+        System.out.println("Inserciones: " + inserciones);   
+    }
 }
--- Proyecto01/Seleccion.java
+++ Proyecto01/Seleccion.java
@@ -1,25 +1,27 @@
 public class Seleccion {  
    
     public static void selectionSort(int arr[]){ 
-        int iteraciones = 0, comparaciones = 0, cambios = 0, inserciones = 0;
+        int accesosArreglo = 0, comparaciones = 0, cambios = 0, inserciones = 0;
 
         int n = arr.length;
-        for (int i = 0; i < n - 1; i++){  
+        for (int i = 0; i < n - 1; i++) {  
             int min = i;
-            inserciones++;  
-            for (int j = i + 1; j < n; j++){  
-                if (arr[j] < arr[min]){  
-                    comparaciones++;
+            accesosArreglo++;  
+            for (int j = i + 1; j < n; j++) {  
+                comparaciones++;
+                if (arr[j] < arr[min]) {  
                     min = j;  
-                    inserciones++;
+                    accesosArreglo++;
                 }                      
             }  
-            Utilerias.intercambiar(arr, i,min);
-            cambios++;  
+            Utilerias.intercambiar(arr, i, min);
+            cambios++;
+            inserciones++;
         }
 
+        System.out.println("Accesos al arreglo: " + accesosArreglo);
         System.out.println("Comparaciones: " + comparaciones);
         System.out.println("Cambios: " + cambios);
-        System.out.println("Inserciones: " + inserciones); 
+        System.out.println("Incersiones: " + inserciones); 
     }  
 }  
