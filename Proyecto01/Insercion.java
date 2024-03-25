fd8cb216ae2e873061eab13714e84f8441c1674e
Azzathoth1126 <alexis.mta2020@gmail.com>
2024-03-25 06:38:49 +0000

    Cambios

--- Proyecto01/Insercion.java
+++ Proyecto01/Insercion.java
@@ -3,20 +3,21 @@ public class Insercion{
     public static void insertionSort(int array[]) {  
         int accesosArreglo = 0, comparaciones = 0, cambios = 0;
 
-        int n = array.length;  
+        int n = array.length; //Tamaño del arreglo
+        
         for (int j = 1; j < n; j++) {  
             int key = array[j];
-            accesosArreglo++;  // Incremento cada vez que se accede al arreglo
-            
             int i = j - 1;  
+
             while ((i > -1) && (array[i] > key)) {  
+
                 comparaciones++; // Incremento por cada comparación de elementos del arreglo
                 array[i + 1] = array[i];  
                 cambios++; // Incremento por cada cambio de elementos
                 i--;  
             }  
             array[i + 1] = key;
-        }  
+        }accesosArreglo++;  // Incremento cada vez que se accede al arreglo
 
         System.out.println("Accesos al arreglo: " + accesosArreglo);
         System.out.println("Comparaciones: " + comparaciones);
