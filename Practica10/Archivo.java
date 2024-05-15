5530008587314f94391e1436cbc6decb44cfa804
Azzathoth1126 <alexis.mta2020@gmail.com>
2024-05-15 21:23:00 +0000

    codigo listo

--- Practica10/Archivo.java
+++ Practica10/Archivo.java
@@ -8,15 +8,23 @@ import java.util.Scanner;
 
 public class Archivo {
     
-    public void crearArchivo(String nombre) throws IOException{
+    public void crearArchivo(File directorio) throws IOException{
+        Scanner scanner = new Scanner(System.in); 
 
-        File archivo = new File (nombre);
+        System.out.println("Ingresa el nombre del archivo con su extencion: ");
+        String nombre = scanner.nextLine();
+
+        File archivo = new File (directorio, nombre);
         
         if (archivo.exists()){
             System.out.println("Ya existe el archivo.\n");
-        } else{
+        } 
+        else{
+
             FileWriter fw = new FileWriter(archivo);
-            System.out.println("Se creó el archivo exitosamente.\n");  
+
+            System.out.println("Se creó el archivo exitosamente.\n");
+            System.out.println("\nArchivo creado en: " + archivo.getAbsolutePath());
             escribir(nombre, false);
             fw.close();      
 
@@ -25,28 +33,43 @@ public class Archivo {
     }
 
 
-    /*public void crearDirectorio(String ubicacion) throws IOException{
-        File director = new File (ubicacion);
-        if (director.exists()){
-            System.out.println("Esta ubicacion ya existe");
-        }else{
+    public void ubicarDirectorio() throws IOException{
+
+        Scanner scanner= new Scanner(System.in);
+        File directorio = new File("/workspace/EDA-II/");
+  
+        System.out.print("\nIngresa el nombre del directorio donde se guardara el archivo ");
+        String nomDiretorio = scanner.nextLine();
+             
+        File directoriaReal = new File(directorio, nomDiretorio);
+
+        if (directoriaReal.exists()) {
+            System.out.println("Directorio ubicado");
+            crearArchivo(directoriaReal);
+        } else {
+            System.out.println("\nEl directorio no existe.");
+            System.out.println("Dame un directorio valido");
+            ubicarDirectorio();
             
         }
-    }*/
+
+    }
     
 
     public void escribir(String nombre, boolean modo) throws IOException{
+
         File archivo = new File (nombre);
+
         if(archivo.exists()){
             FileWriter fw = new FileWriter(archivo, modo);
             Scanner sc= new Scanner(System.in);
             String cadena;
+
             System.out.println("Añadir contenido: ");
             cadena=sc.nextLine();
             fw.write(cadena);
             fw.close();
-        }
-        else{
+        }else{
             System.out.println("No existe el archivo.");
         }
         
@@ -54,11 +77,12 @@ public class Archivo {
     
 
     public void eliminar(String nombre) throws IOException{
+
         File archivo = new File (nombre);
+
         if(archivo.exists()){
             archivo.delete();
-        }
-        else{
+        }else{
             System.out.println("No existe el archivo.");
         }
     }
@@ -67,6 +91,7 @@ public class Archivo {
     public void info(String nombre) throws IOException{
 
         File archivo = new File (nombre);
+
         if (archivo.exists()){
 
             System.out.println("\nNombre de Archivo: " + archivo.getName());
--- Practica10/Practica10EDA.java
+++ Practica10/Practica10EDA.java
@@ -8,47 +8,45 @@ public class Practica10EDA{
     
     public static void main(String[] args) throws IOException {
         
-        Scanner sc = new Scanner (System.in);
-        int o;
+        Scanner scanner = new Scanner (System.in);
+        int opcion;
         String nombre;
         Archivo arch = new Archivo();
         System.out.println("Archivos :D");
         do{
             System.out.println("\n1.Crear Archivo\n2.Sobreescribir en el archivo\n3.Añadir contenido en el archivo\n4.Informacion del archivo\n5.Elimina el archivo\n6.Salir\n");
-            o=sc.nextInt();
-            switch(o){
+            opcion=scanner.nextInt();
+            switch(opcion){
                 case 1:
-                    System.out.println("Ingresa el Nombre del archivo con su extension: ");
-                    nombre=sc.next();
-                    arch.crearArchivo(nombre);
+                    arch.ubicarDirectorio();
                     break;
                 case 2:
                     System.out.println("Ingresa el Nombre del archivo con su extension:");
-                    nombre=sc.next();
+                    nombre=scanner.next();
                     arch.escribir(nombre, false);
                     break;
                 case 3:
                     System.out.println("Ingresa el Nombre del archivo con su extension:");
-                    nombre=sc.next();
+                    nombre=scanner.next();
                     arch.escribir(nombre, true);
                     break;
                 case 4:
                     System.out.println("Ingresa el Nombre del archivo con su extension:");
-                    nombre=sc.next();
+                    nombre=scanner.next();
                     arch.info(nombre);
                     break;
                 case 5:
                     System.out.println("Ingresa el Nombre del archivo con su extension:");
-                    nombre=sc.next();
+                    nombre=scanner.next();
                     arch.eliminar(nombre);
                     break;
                 default:
-                    if (o!=6){
+                    if (opcion!=6){
                         System.out.println("Opción inválida.");
                     }
                     break;
             }
-        }while(o!=6);
+        }while(opcion!=6);
     }
     
 }
