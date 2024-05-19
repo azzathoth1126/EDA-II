54840db3feadb1ff21e27c0084ba77abf1da6215
Azzathoth1126 <alexis.mta2020@gmail.com>
2024-05-19 23:26:09 +0000

    subiendo cosas alazar

--- Practica11_12_13/lab12/act03b.c
+++ Practica11_12_13/lab12/act03b.c
@@ -20,12 +20,10 @@ int main(int argc, char* argv[]){
     step= 1.0/(double)num_steps;
     empezar = omp_get_wtime();
 
-    #pragma omp parallel 
-    {
-        for(i=0; i<num_steps; i++){
-            x = (i + 0.5)*step;
-                sum = sum + 4.0/(1.0 + x*x);
-        }
+    #pragma omp for 
+    for(i=0; i<num_steps; i++){
+        x = (i + 0.5)*step;
+            sum = sum + 4.0/(1.0 + x*x);
     }
 
     pi = sum*step;
@@ -35,5 +33,4 @@ int main(int argc, char* argv[]){
     printf("El tiempo de calculo del numero pi es: %1f segundos\n", terminar-empezar);
 
     return 0;
-
 }
