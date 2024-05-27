4d91c078959ee433d83f295c170e7297dfffcd00
Azzathoth1126 <alexis.mta2020@gmail.com>
2024-05-27 07:25:09 +0000

    Practica LIsta

--- Practica05/HashModulo.java
+++ Practica05/HashModulo.java
@@ -1,8 +1,156 @@
+
+import java.util.Scanner;
+
 public class HashModulo {
 
-    public static void tabHash() {
-        int[][] tabla = new int[18][18];
+    public static final int nElem = 20;
+
+    public static void main(String []args){
+
+        Scanner scan = new Scanner(System.in);
+
+        
+        
+        int[][] tablaHas = new int[nElem][2];
+
+        for(int i=0; i<nElem; i++){
+            for(int j=0; j<1; j++){
+                tablaHas[i][j] = 0;
+            }
+            tablaHas[i][0] = i+1;
+        }
+
+        boolean condition = true;
+        int opcion;
+        int num;
+
+        System.out.println("\n    ******Menu******    ");
+
+        while (condition) {
+            System.out.println("\n1. Agregar elementos");
+            System.out.println("2. Imprimir lista");
+            System.out.println("3. Buscar elemento");
+            System.out.println("4. Eliminar elemento");
+            System.out.println("5. Salir");
+
+            System.out.println("\nQue deseas hacer: ");
+            opcion = scan.nextInt();
+            scan.nextLine();
+
+            switch (opcion) {
+                case 1:
+                    System.out.print("\nDame un numero de 9 digitos: ");
+                    num = scan.nextInt();
+                    add(tablaHas, num);
+                    break;
+
+                case 2:
+                    cleanScrean();
+
+                    System.out.print("\n");
+                    imprimir(tablaHas, 0, true);
+                    break;
+
+                case 3:
+                    System.out.print("\nDame la llave del elemento que buscas: ");
+                    int keyS = scan.nextInt();
+                    imprimir(tablaHas, keyS, false);
+                    break;
+
+                case 4:
+                    System.out.print("\nQue numero de llave quieres eliminar: ");
+                    int keyR = scan.nextInt();
+                    remov(tablaHas, keyR);
+                    break;
+
+                case 5:
+                    condition = false;
+                    System.out.println("\nSaliendo");
+                    break;
+            
+                default:
+                    System.out.println("\nEscoge una opción valida");
+                    break;
+            }
+
+        }
+        scan.close();
+    }
+
+    private static void add(int[][] arr, int val) {
+        String cadena = Integer.toString(val);
+
+        int num1 = Integer.parseInt(cadena.substring(0, 3));
+        int num2 = Integer.parseInt(cadena.substring(3, 6));
+        int num3 = Integer.parseInt(cadena.substring(6, 9));
+
+        int num4 = num1+num2+num3;
+
+        num4 = num4%17;
+
+        fPlegamiento(arr, val, num4);
 
     }
 
+
+    private static void imprimir(int[][] arr, int key, boolean modo){
+        if (modo){
+            for(int i=0; i<nElem; i++){
+                if(arr[i][1] != 0){
+                    System.out.println(" { " +arr[i][0]+ ", " +arr[i][1]+ "}");
+                }
+                
+            }
+        } else{
+            if (arr[key][1] == 0) {
+                System.out.println("\nEl elemento no existe\n");
+            } else System.out.println("\nElemento * " +arr[key][1]+ " * encontrado");
+        }
+        
+    }
+
+
+    private static void remov(int[][] arr, int key){
+        arr[key][0] = 0;
+        arr[key][1] = 0;
+        System.out.println("\nElemento eliminado");
+
+    }
+
+
+    private static void fPlegamiento(int[][] arr, int val, int mod){
+        if(arr[mod][1] == 0){
+            arr[mod][1] = val;
+            arr[mod][0] = mod;
+            System.out.println("\n " + val + " agregado en en la posicion: " + mod);
+
+        } else if(mod>nElem){
+            
+            while(mod>nElem){
+                mod--;
+            }
+            System.out.println("\nOcurrio una colision, arreglando el problema");
+            fPlegamiento(arr, val, mod);
+            
+        } else if(mod<nElem){
+            if(mod >= 0){
+                while(arr[mod][1] != 0){
+                    mod--;
+                }
+
+                arr[mod][1] = val;
+                arr[mod][0] = mod;
+                System.out.println("\n " + val + " agregado en en la posicion: " + mod);
+
+            } else {
+                System.out.println("\nTablaHash llena \nElimina una clave");
+            }
+        }
+    }
+
+
+    private static void cleanScrean() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
 }
--- Practica05/Principal.java
+++ Practica05/Principal.java
@@ -1,33 +1,75 @@
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+/*
 import java.util.Scanner;
+*/
+import java.util.Set;
 
 public class Principal{
 
     public static void main(String []args) {
 
         //Variable mp que se inicializa con una instancia de HashMap<String, Integer>.
-        Map<String,Integer> mp = new HashMap<>();
-        Scanner scanner = new Scanner(System.in);
+        HashMap<String,Integer> mp = new HashMap<>();
 
-        //isEmpty() Devuelve true si la tabla esta vacio y false si la tabla tiene un elemento
-        System.out.println("\nLa tabla contiene elementos: " + !mp.isEmpty());
-        System.out.println("\nCuanto elementos contiene la tabla: " + mp.size());
+        //Obtiene toda la colección de valores del HashMap
+        Collection<Integer> valores = mp.values();
+
+        //Obtiene toda la colección de claves del HashMap
+        Set<String> claves = mp.keySet();
+
+        System.out.println("\nTabla Hash creada");
 
         // Añadiendo elementos en la tabla Hash
         mp.put("Alexis", 22);
-        mp.put("Maria", 43); 
+        mp.put("Maria", 33); 
         mp.put("Armando", 10);
         mp.put("Luz", 20);
-        mp.put("Erina", 30);      
-
+        mp.put("Erina", 30); 
+        mp.put("Karla", 23); 
+        mp.put("Fernando", 19); 
+        mp.put("Jonathan", 21); 
+        mp.put("Paola", 23); 
+        mp.put("Ana", 22); 
         System.out.println("\nElementos Agregados");  
 
-        //Numeros de elementos en la tabla Hash
-        System.out.println("\nCuanto elementos contiene la tabla: " + mp.size());
 
-        //Imprimiendo elementos de la tabla Hash
-        imprimir(mp);
+        //Verificando si la tabla esta vacia y cuantos elementos tiene
+        if(!mp.isEmpty()){
+            System.out.println("\nLa tabla si tiene elementos");
+            System.out.println("Cuanto elementos contiene la tabla: " + mp.size());
+
+            imprimir(mp); //Imprimiendo la tabla Hash
+        }
+        
+
+        //Obteniendo datos de los elementos, agregando elementos y eliminando
+        System.out.println("\nCual es la edad de Alexis " + mp.get("Alexis"));
+        mp.put("Tere", 45);
+        mp.remove("Ana");
+        System.out.println("Cambios efecutados\n");
+        imprimir(mp); //Imprimiendo la tabla Hash
+
+
+        //Comparando elementos
+        if (mp.get("Alexis").equals(mp.get("Tere"))){
+            System.out.println("\nLos valores son iguales");
+        }else System.out.println("\nLos elementos no son iguales");
+
+
+        // Iterar los valores y los mostrarlos
+        System.out.println("\nValores en el mapa:");
+        for (Integer valor : valores) {
+            System.out.println(valor);
+        }
+
+
+        //Itera las claves y las muestra
+        System.out.println("\nClaves en el mapa:");
+        for (String clave : claves) {
+            System.out.println(clave);
+        }
 
         //containKey() Busca en la tabla si existe dicho DATO o NOMBRE o LLAVE
         System.out.println("\nExiste Alexis en la tabla: " + mp.containsKey("Alexis"));
@@ -35,7 +77,7 @@ public class Principal{
         //containValue() Busca en la tabla si existe dicho DATO o EDAD o VALOR 
         System.out.println("\nAlguien tiene la edad de 79 años en la tabla: " + mp.containsValue(79));
         
-        System.out.println("\nCual es la edad de Alexis " + mp.get("Alexis"));
+        
 
     }
 
@@ -43,7 +85,7 @@ public class Principal{
     public static void imprimir(Map<String,Integer> elemento) {
         System.out.println("\nTabla Hash: ");
         for(Map.Entry<String,Integer>entry : elemento.entrySet()){
-            System.out.println("Nombre: " + entry.getKey() + ", Altura: " + entry.getValue());
+            System.out.println("Nombre: " + entry.getKey() + ", Edad: " + entry.getValue());
         }
     }
 
@@ -51,11 +93,13 @@ public class Principal{
 
 
 /* 
-        mp.put(3, p); // Añadimos un objeto persona al map
-        p = new Persona("Yolanda",174);
-
-        mp.put(4, p); // Añadimos un objeto persona al map
-        p = new Persona("María Dolores",169);
-
-        mp.put(4, p); // Esto crea una colisión ¡Dos objetos no pueden tener la misma clave!
+         •• containsKey
+         •• containsValue
+         •• equals
+         •• get, put, remove
+         •• size
+        • otros 3 métodos de tablas hash y explicar su funcionamiento:
+            isEmpty()
+            Collection<V> values()
+            Collection<C> claves()
 */ 
